20d0bf7bd80979449f59c1ce24997806b34e9636
AlexRov <mail.ponomarev@yandex.ru>
2020-09-26 16:51:38 +0300

    feat: add docker

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -1,22 +1,15 @@
 import React              from 'react'
-import { ThemeProvider }  from 'emotion-theming'
-import { IntlProvider }   from 'react-intl'
 
 import { Column, Layout } from '@ui/layout'
 import { NextLink }       from '@ui/link'
-import { theme }          from '@ui/theme'
 
 export default () => (
   <>
-    <IntlProvider locale='ru' defaultLocale='ru'>
-      <ThemeProvider theme={theme}>
-        <Column alignItems='center'>
-          <Layout flexBasis={50} />
-          <NextLink href='/signin'>Авторизироваться</NextLink>
-          <Layout flexBasis={50} />
-          <NextLink href='/auth'>Зарегистрироваться</NextLink>
-        </Column>
-      </ThemeProvider>
-    </IntlProvider>
+    <Column alignItems='center'>
+      <Layout flexBasis={50} />
+      <NextLink href='/signin'>Авторизироваться</NextLink>
+      <Layout flexBasis={50} />
+      <NextLink href='/auth'>Зарегистрироваться</NextLink>
+    </Column>
   </>
 )
---
+++ frontend/app/src/messages.ts
@@ -0,0 +1,15 @@
+import { defineMessages } from 'react-intl'
+
+import { name as scope }  from '../package.json'
+
+export default defineMessages({
+  title: {
+    id: `${scope}.title`,
+    defaultMessage: 'Чат',
+  },
+
+  description: {
+    id: `${scope}.description`,
+    defaultMessage: 'Чат',
+  },
+})
